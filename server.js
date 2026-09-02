b3e2cc8a2552b0099c091c3389f32ca450090d2b
vnn <vnn@debian.debian>
2026-09-02 12:52:53 +0300

    test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 const app = express();
 
-app.get("", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("Hello World");
 });
 
