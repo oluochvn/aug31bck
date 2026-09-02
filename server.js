99e8ade5688a970fbc0d4ab4cc66789e654623f6
vnn <vnn@debian.debian>
2026-09-02 12:32:50 +0300

    setup1

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import mongoose from "mongoose";
+const app = express();
+
+app.get("", (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
